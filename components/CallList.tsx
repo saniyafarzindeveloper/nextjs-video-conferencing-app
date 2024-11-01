94d006b0c7e7baa0e80d3c539335f35c46587135
saniyafarzindeveloper <developerfarzin@gmail.com>
2024-11-01 20:48:10 +0530

    get calls logic

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,7 +1,53 @@
+'use client'
+import { useGetCalls } from "@/hooks/useGetCalls"
+import { CallRecording } from "@stream-io/node-sdk";
+import { Call } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 
 export default function CallList({type}: {type: 'upcoming' | 'ended' | 'recordings'}) {
+  const {endedCalls, upcomingCalls, isLoading, callRecordings} = useGetCalls();
+  const router = useRouter();
+  const [recordings, setRecording] = useState<CallRecording[]>([]);
+
+  //call based calling
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings' :
+      return recordings; 
+      case 'upcoming':
+        return upcomingCalls;
+    
+      default:
+       return [];
+    }
+  }
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'No previous calls';
+      case 'recordings' :
+        return 'No recordings';
+      case 'upcoming':
+        return 'No upcoming calls';
+    
+      default:
+       return "";
+    }
+  }
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
   return (
-    <div>CallList</div>
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+        {/* {calls && calls.length > 0 ? calls.map((meeting : Call | CallRecording) => (
+          <MeetingCard />
+        ))} */}
+    </div>
   )
 }
--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -1,28 +1,72 @@
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useGetCalls = () =>{
-    const [calls, setCalls] = useState<Call[]>([]); //call is the type
-    //keeping track of the loading state
-    const [isLoading, setIsLoading] = useState(false);
-    //fetching calls
-    const client = useStreamVideoClient();
-    //calls should be fetched for a particular user
-    const {user} = useUser(); //getting users from clerk
+export const useGetCalls = () => {
+  const [calls, setCalls] = useState<Call[]>([]); //call is the type
+  //keeping track of the loading state
+  const [isLoading, setIsLoading] = useState(false);
+  //fetching calls
+  const client = useStreamVideoClient();
+  //calls should be fetched for a particular user
+  const { user } = useUser(); //getting users from clerk
 
-    useEffect(() => {
-        //createing an async function to fetch calls
-        const loadCalls = async () =>{
-            if(!client || !user?.id) return;
+  useEffect(() => {
+    //creating an async function to fetch calls
+    const loadCalls = async () => {
+      if (!client || !user?.id) return;
 
-            setIsLoading(true);
-            // try {
-                
-            // } catch (error) {
-                
-            // }
-        }
-    }, [client, user?.id]); //need both to fetch calls
+      setIsLoading(true);
+      try {
+        const { calls } = await client.queryCalls({
+          sort: [
+            {
+              field: "starts_at",
+              direction: -1, //sort by start date
+            },
+          ],
+          filter_conditions: {
+            //show the call if i have created it or if we're the member in the call
+            starts_at: { $exists: true }, //checking if call exists
+            $or: [
+              {
+                created_by_user_id: user.id, //currently logged in user
+              },
+              {
+                members: { $in: [user.id] },
+              },
+            ],
+          },
+        }); //passing opt object
 
-}
+        setCalls(calls);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadCalls();
+  }, [client, user?.id]); //need both to fetch calls
+
+  const now = new Date();
+
+  //filtering out calls - ended/upcoming/recording
+
+  //logic for finding ended calls
+  const endedCalls = calls.filter(({ state: { startsAt, endedAt } }: Call) => {
+    return (startsAt && new Date(startsAt) < now) || !!endedAt; //date atm is lesser then the call is already ended (tym has already paased)
+  });
+
+  //logic for finding new/upcoming calls
+  const upcomingCalls = calls.filter(({ state: { startsAt } }: Call) => {
+    return startsAt && new Date(startsAt) > now; //date atm is graeter then the call is upcoming (yet to happen)
+  });
+
+  return {
+    endedCalls,
+    upcomingCalls,
+    callRecordings: calls,
+    isLoading,
+  };
+};
